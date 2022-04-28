cc60cc8b7fccd50ff437f73801b33e582f584e1a
Axfez <santimon72@gmail.com>
2022-04-27 20:52:06 -0500

    project set up

---
+++ src/variables.js
@@ -0,0 +1,12 @@
+export const colors = {
+    lightCyan: 'hsl(193, 38%, 86%)',
+    neonGreen: 'hsl(150, 100%, 66%)',
+    grayishBlue: 'hsl(217, 19%, 38%)',
+    darkGrayishBlue: 'hsl(217, 19%, 24%)',
+    darkBlue: 'hsl(218, 23%, 16%)'
+}
+
+export const fonts = {
+    quote: '28px',
+    Weights: 800
+}
