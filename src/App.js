ff82dcb1a57329310fcd4cce6d61e258b51e6678
Axfez <santimon72@gmail.com>
2022-04-27 20:50:08 -0500

    asd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Box } from '@mui/material'
+import { colors, fonts } from './variables'
+import { useEffect, useState } from 'react'
+import { Dice, DividerDesk, DividerMob } from './svg'
 
 function App() {
+
+  const [text, setText] = useState([])
+
+  const fetchAdvice = async () => {
+    const res = await fetch('https://api.adviceslip.com/advice')
+    const data = await res.json()
+
+    console.log(data)
+
+    setText(data.slip)
+  }
+
+  useEffect(() => { fetchAdvice() }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      position: 'relative',
+      bgcolor: `${colors.darkGrayishBlue}`,
+      padding: '20px 10px 40px',
+      borderRadius: '20px',
+      letterSpacing: '6px'
+    }}>
+      <Box sx={{
+        color: `${colors.neonGreen}`,
+        fontSize: 21,
+      }}>
+        ADVICE #{text.id}
+      </Box>
+      <Box sx={{
+        color: '#fff',
+        fontWeight: `${fonts.Weights}`,
+        fontSize: `${fonts.quote}`,
+        paddingBottom: '40px',
+      }}>
+        “{text.advice}”
+      </Box>
+      <Box as='img' />
+      <DividerDesk />
+
+      <Box sx={{
+        display: 'flex',
+        cursor: 'pointer',
+        width: '80px',
+        height: '80px',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: '-40px',
+        bgcolor: `${colors.neonGreen}`,
+        borderRadius: '50%',
+      }}>
+        <Dice />
+      </Box>
+    </Box>
+
   );
 }
 
